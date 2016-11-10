b6fd6506334d940a5c3d69925dc07dd8dcff1147
Tu Chu Quang <tucq88@gmail.com>
2016-11-10 14:04:11 +0700

    Add feature B (#5)
    * Add feature B
    
    * Feature B Update
    
    * Feature B Update 2
    
    * Add failed feature B
    
    * Fix bug

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -16,4 +16,10 @@ class ExampleTest extends TestCase
         $this->visit('/')
              ->see('Laravel');
     }
+
+    public function testFeatureB()
+    {
+        $this->visit('/')
+             ->see('Feature B');
+    }
 }
