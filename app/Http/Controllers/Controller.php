5b18ea7e544ac22c45d63892de382e02bf4b5de6
Tucq <tucq88@gmail.com>
2016-11-16 13:49:42 +0700

    Add cop pate code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,24 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function commonFunc() {
+
+    }
+
+    public function doSomething() {
+        $this->commonFunc();
+        for ($i = 0; $i <= 10; ++$i) {
+            $i++;
+        }
+        return 'aaaa';
+    }
+
+    public function doOtherThing() {
+        $this->commonFunc();
+        for ($i = 0; $i <= 10; ++$i) {
+            $i++;
+        }
+        return 'aaaa';
+    }
 }
